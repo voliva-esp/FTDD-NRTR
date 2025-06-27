bfdaf180e82afb742955b515cbfc8d7247448af8
romOlivo <al341918@uji.es>
2025-06-27 17:05:16 +0200

    Improved executions for better measuring the memory

--- source/Exec/run.bash
+++ source/Exec/run.bash
@@ -1,27 +1,43 @@
 #!/bin/bash
-tools=('GTN' 'FTDD')
+tools=('FTDD')
+#paths=('qft_indep' 'rqc' 'ghz' 'qnn' 'graph_state' 'qwalk' 'real_amplitude' 'qpe' 'amplitude_estimation' 'grover')
+#circuit=('qftentangled_indep_qiskit' 'rqc' 'ghz' 'qnn_indep_tket' 'graph_state' 'qwalk_d1' 'realamprandom_indep_qiskit' 'qpeexact_indep_qiskit' 'ae_indep_qiskit' 'grover-v-chain_ind>
+#minQubits=(10 1 20 5 5 3 2 2 2 2)
+#maxQubits=(129 25 129 50 50 15 130 130 130 17)
+methods=('seq')
 paths=('qft_indep' 'rqc' 'ghz')
 circuit=('qftentangled_indep_qiskit' 'rqc' 'ghz')
-minQubits=(2 1 5)
-maxQubits=(3 12 6)
+minQubits=(10 1 20)
+maxQubits=(12 2 22)
 echo "Starting all executions..."
 for (( j=0; j<${#tools[@]}; j++))
 do
   echo " > Running ${tools[j]} backend..."
-  for method in k-ops seq
+  for (( l=0; l<${#methods[@]}; l++))
   do
-    echo " --- Running for $method contraction method..."
-    for (( i=0; i<${#paths[@]}; i++))
-    do
-      echo " # --- Running ${circuit[i]} circuit type..."
-      python3 source/Exec/runCircuit.py ${minQubits[i]} ${maxQubits[i]} $method ${paths[i]} ${circuit[i]} ${tools[j]}
-      echo " # --- Circuit done"
+    echo " --- Running for ${methods[l]} contraction method..."
+      for (( i=0; i<${#paths[@]}; i++))
+      do
+        echo " ## --- Running ${circuit[i]} circuit type..."
+        for (( n=${minQubits[i]}; n<${maxQubits[i]}+1; n++))
+        do
+          echo " >>> ## --- N: $n"
+          timeout 10 python3 source/Exec/runCircuit.py ${n} ${methods[l]} ${paths[i]} ${circuit[i]} ${tools[j]}
+          exit_code=$?
+          if [ "$exit_code" -ne 0 ]; then
+            if [ "$exit_code" -eq 124 ]; then
+              echo " ## --- Circuit failed (timeout)"
+            else
+              echo " ## --- Circuit failed"
+            fi
+            break
+          else
+            echo " ## --- Circuit done"
+          fi
+        done
     done
     echo " --- Contraction method done"
   done
   echo "> Backend done"
 done
 echo "Execution ended successfully"
-
-
-
--- source/Exec/runCircuit.py
+++ source/Exec/runCircuit.py
@@ -2,55 +2,33 @@ from qiskit import QuantumCircuit
 
 from source.TDD_Q import simulate
 
-
-class CircuitGroupRun:
-    def __init__(self, path, circuit_name, contraction_method, backend="PyTDD", use_tetris=False, use_slicing=False,
-                 slicing_method="max", n_indices=1, prefix_circuit_name=""):
-        self.path = path
-        self.backend = backend
-        self.n_indices = n_indices
-        self.use_tetris = use_tetris
-        self.use_slicing = use_slicing
-        self._circuit_name = circuit_name
-        self.slicing_method = slicing_method
-        self.contraction_method = contraction_method
-        self.prefix_circuit_name = prefix_circuit_name
-
-        self.is_input_closed = True
-        self.is_output_closed = False
-
-    def _run_single_circuit(self, n):
-        file_name = f"{self._circuit_name}_{n}{self.prefix_circuit_name}"
-        circuit = QuantumCircuit.from_qasm_file(self.path + file_name + '.qasm')
-        circuit.name = self._circuit_name
-        result = simulate(circuit, use_slicing=self.use_slicing, contraction_method=self.contraction_method,
-                          is_input_closed=self.is_input_closed, is_output_closed=self.is_output_closed,
-                          use_tetris=self.use_tetris, n_indices=self.n_indices, slicing_method=self.slicing_method,
-                          backend=self.backend, handler_name="file")
-
-    def run(self, first_n_qubits, last_n_qubits):
-        for n_qubits in range(first_n_qubits, last_n_qubits):
-            self._run_single_circuit(n_qubits)
-        if last_n_qubits > first_n_qubits:
-            self._run_single_circuit(last_n_qubits)
-
-
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 6:
         raise ValueError("Incorrect usage of runCircuit file. \n Correct usage: \n   $ python runCircuit.py " +
-                         "<min_n_qubits> <max_n_qubits> <contraction_method> <folder_name> <circuit_name> (<tool>)" +
+                         "<n_qubits> <contraction_method> <folder_name> <circuit_name> (<tool>)" +
                          "  These are the arguments you used: \n  " + str(sys.argv))
-    min_qubits = int(sys.argv[1])
-    max_qubits = int(sys.argv[2])
-    contraction_method = sys.argv[3]
-    folder_name = sys.argv[4]
-    circuit_name = sys.argv[5]
+    n_qubits = int(sys.argv[1])
+    contraction_method = sys.argv[2]
+    folder_name = sys.argv[3]
+    circuit_name = sys.argv[4]
     tool = "PyTDD"
-    if len(sys.argv) == 7:
-        tool = sys.argv[6]
-    cgr = CircuitGroupRun(path=f"./Benchmarks/MQTbench/{folder_name}/", circuit_name=circuit_name,
-                          contraction_method=contraction_method, backend=tool)
-    cgr.run(min_qubits, max_qubits)
+    if len(sys.argv) == 6:
+        tool = sys.argv[5]
+    use_slicing = False
+    is_input_closed = True
+    is_output_closed = False
+    use_tetris = False
+    n_indices = 0
+    slicing_method = "max"
+    path=f"./Benchmarks/MQTbench/{folder_name}/"
+    file_name = f"{circuit_name}_{n_qubits}"
+    circuit = QuantumCircuit.from_qasm_file(path + file_name + '.qasm')
+    circuit.name = circuit_name
+
+    result = simulate(circuit, use_slicing=use_slicing, contraction_method=contraction_method,
+                      is_input_closed=is_input_closed, is_output_closed=is_output_closed,
+                      use_tetris=use_tetris, n_indices=n_indices, slicing_method=slicing_method,
+                      backend=tool, handler_name="hybrid")
 
 
